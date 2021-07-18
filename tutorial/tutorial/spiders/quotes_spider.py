4bdb942edff463461f406cb5304f2557d6165d9a
opusvar <sprojdev@gmail.com>
2021-07-18 19:13:44 -0400

    refactored spider to yield a dictionary of quotes

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -2,18 +2,15 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes" #identifies the spider, must be unique in the project
-
-    def start_requests(self): #function that returns an iterable (list or generator), starts the chain of requests
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
 
     def parse(self, response): # parsing logic for the response and data capture.  set this to be JSON, text, or XML
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+        for quote in response.css("div.quote"):
+            yield {
+                "text": quote.css("span.text::text").get(),
+                "author": quote.css("small.author::text").get(),
+                "tags": quote.css("div.tags a.tag::text").getall(),
+            }
