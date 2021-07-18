39659a146cfeb2e26396b8030bd2681a2e313066
opusvar <sprojdev@gmail.com>
2021-07-18 19:24:47 -0400

    Added annotations

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -14,3 +14,8 @@ class QuotesSpider(scrapy.Spider):
                 "author": quote.css("small.author::text").get(),
                 "tags": quote.css("div.tags a.tag::text").getall(),
             }
+
+        next_page = response.css("li.next a::attr(href)").get() # creates an object called next page, focused on a list element with the class next, and finds the attribute href
+        if next_page is not None: # if the get returns a resonse
+            next_page = response.urljoin(next_page) # update the next_page variable with the new url
+            yield scrapy.Request(next_page, callback=self.parse) # yield a generator object that has the new page information
