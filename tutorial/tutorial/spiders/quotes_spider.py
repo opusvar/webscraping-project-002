fc3470b2a58f1682ed5ecaa92b79edd47dc687e1
opusvar <sprojdev@gmail.com>
2021-07-19 09:28:36 -0400

    refactored quotes spider to accept custom css tags through command line arguements

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -2,26 +2,37 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes" #identifies the spider, must be unique in the project
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
-    ]
+    
+    def start_requests(self):
+        url = "http://quotes.toscrape.com/"
+        tag = getattr(self, "tag", None)
+        if tag is not None:
+            url = url + "tag/" + tag
+        yield scrapy.Request(url, self.parse)
 
     def parse(self, response): # parsing logic for the response and data capture.  set this to be JSON, text, or XML
         for quote in response.css("div.quote"):
             yield {
                 "text": quote.css("span.text::text").get(),
                 "author": quote.css("small.author::text").get(),
-                "tags": quote.css("div.tags a.tag::text").getall(),
             }
+
+    
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+
+# alternative ways to move to another page
+
+    # verbose way to move to another page
+       
+        """
 
         next_page = response.css("li.next a::attr(href)").get() # creates variable called next page, focused on a list element with the class next, and finds the attribute href
         if next_page is not None: # if the get returns a response
             next_page = response.urljoin(next_page) # update the next_page variable with the new absolute url
             yield scrapy.Request(next_page, callback=self.parse) # yield a generator that has the new page information
-
-# alternative ways to move to another page
-
+        """
 
     # response.follow() with variable
 
