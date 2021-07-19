4ceeabee3cda7f544c8065928631194572d71b2a
opusvar <sprojdev@gmail.com>
2021-07-19 08:08:10 -0400

    Alternative ways to follow response objects and multiple objects

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -15,7 +15,42 @@ class QuotesSpider(scrapy.Spider):
                 "tags": quote.css("div.tags a.tag::text").getall(),
             }
 
-        next_page = response.css("li.next a::attr(href)").get() # creates an object called next page, focused on a list element with the class next, and finds the attribute href
-        if next_page is not None: # if the get returns a resonse
-            next_page = response.urljoin(next_page) # update the next_page variable with the new url
-            yield scrapy.Request(next_page, callback=self.parse) # yield a generator object that has the new page information
+        next_page = response.css("li.next a::attr(href)").get() # creates variable called next page, focused on a list element with the class next, and finds the attribute href
+        if next_page is not None: # if the get returns a response
+            next_page = response.urljoin(next_page) # update the next_page variable with the new absolute url
+            yield scrapy.Request(next_page, callback=self.parse) # yield a generator that has the new page information
+
+# alternative ways to move to another page
+
+
+    # response.follow() with variable
+
+        """
+        next_page = response.css('li.next a::attr(href)').get() # creates a variable called next page, focused on a list element with the calss next and find the attribute href
+        if next_page is not None: # if the get returns a repsonse
+            yield response.follow(next_page, callback=self.parse) # yield a request instance (needs to be called) that has the new page information using a relative url
+        """
+    # response.follow() with selector instead of variable
+
+        """
+        for href in response.css("ul.pager a::attr(href)"):
+            yield response.follow(href, callback=self.parse)
+        """
+    
+    # response.follow() with short cut because it's an <a> tag
+
+        """
+        for a in response.css("ul.pager a"):
+            yield response.follow(a, callback=self.parse
+        """
+
+    # response.follow() with multiple instances to follow
+        """
+        anchors = response.css('ul.pager a')
+        yield from response.follow_all(anchors, callback=self.parse)
+        """
+
+    # shortest response.parse with multiple instances
+        """
+        yield from response.follow_all(css="ul.pager a", callback=self.parse)
+        """
